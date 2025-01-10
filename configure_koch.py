458312727bbdcc102b152924b67ebdefbe13249d
Siddarth Narasimhan <sidarceus@gmail.com>
2025-01-09 20:37:43 -0500

    major restructure to functions, include get and set for end effector

--- configure_koch.py
+++ configure_koch.py
@@ -7,6 +7,7 @@ import subprocess
 import cv2
 import time
 import numpy as np
+from scipy.spatial.transform import Rotation as R
 from pynput import keyboard
 from roboticstoolbox import DHRobot, RevoluteDH
 
@@ -23,7 +24,7 @@ class KochRobot:
         subprocess.run(command, check=True, text=True, capture_output=True)
 
         follower_arm = DynamixelMotorsBus(
-            port="/dev/ttyACM0",
+            port=port,
             motors={
                 "shoulder_pan": (1, "xl430-w250"),
                 "shoulder_lift": (2, "xl430-w250"),
@@ -39,8 +40,10 @@ class KochRobot:
             follower_arms={"main": follower_arm},
             calibration_dir=".cache/calibration/koch",
         )
+        self.robot.connect()
+        print("Koch Connected!")
 
-        # DH parameters
+        # DH parameters and limits
         self.d1, self.a2, self.a3, self.d5 = 5.5, 10.68, 10, 10.5
         A = [0, self.a2, self.a3, 0, 0]
         ALPHA = [-np.pi/2, 0, 0, np.pi/2, 0]
@@ -53,61 +56,63 @@ class KochRobot:
         # Initial EE angles
         self.q4, self.q5 = 0, np.pi / 2
         
+        # For computing forward kinematics
         self.robot_dh = DHRobot([RevoluteDH(a=A[i], 
                                             alpha=ALPHA[i], 
                                             d=D[i], 
                                             offset=OFFSET[i], 
                                             qlim=QLIM[i]) for i in range(len(A))])
 
-        self.robot.connect()
+        # Initialize with torch enabled/disabled
+        torque_mode = TorqueMode.ENABLED.value if torque else TorqueMode.DISABLED.value
+        self.robot.follower_arms["main"].write("Torque_Enable", torque_mode)
 
-        if torque:
-            self.robot.follower_arms["main"].write("Torque_Enable", TorqueMode.ENABLED.value)
-        else:
-            self.robot.follower_arms["main"].write("Torque_Enable", TorqueMode.DISABLED.value)
+        # Get current_position
+        self.curr_position = self.get_ee_pos()
         
-        print("Robot connected!")
 
-        # Camera-robot calibration variables
-        self.R, self.t = [], []
+    def get_arm_joint_angles(self):
+        """
+        Reads current joint angles of each revolute joint.
+        """
+        angles = self.robot.follower_arms["main"].read("Present_Position")
+        angles = np.array(angles) /  360 * 2 * np.pi
+        return angles
 
 
-    def read_pos(self, iters):
+    def get_ee_pose(self):
+        """
+        Reads joint angles and computes ee pose.
+        """
 
-        for _ in range(iters):
+        # Follower arms outputs 6 positions because there are 6 motors
+        robot_angles_full = self.get_arm_joint_angles()
 
-            # Follower arms outputs 6 positions because there are 6 motors
-            robot_angles = self.robot.follower_arms["main"].read("Present_Position")
+        # However, we only need the first five for DH - the last one is gripper open/close
+        robot_angles = np.array(robot_angles_full)[:5]
 
-            # However, we only need the first five for DH - the last one is the end effector DOF
-            robot_angles_rad = np.array(robot_angles)[:5] / 360 * 2 * np.pi
+        # Between self.robot and self.robot_dh, the difference is that first and second angle is flipped
+        # Note that first angle is flipped to ensure right-handed axis
+        robot_angles *= np.array([-1, -1, 1, 1, 1])
 
-            # Between self.robot and self.robot_dh, the difference is that the second angle is flipped
-            robot_angles_rad *= np.array([-1, -1, 1, 1, 1])
+        T = np.array(self.robot_dh.fkine(robot_angles))
+        ee_matrix, ee_pos = T[:3, :3], T[:3, 3]
+        quat_xyzw = R.from_matrix(ee_matrix).as_quat()
 
-            out = self.robot_dh.fkine(robot_angles_rad)
-            target_position = np.array(out)[:3, 3]
-            print("Position:", target_position)
-
-            # Verify correctness of inverse kinematics
-            Q_after = self.inv_kin(target_position)
-            print("Error:", np.linalg.norm(Q_after[:4]-robot_angles_rad[:4]))
-            time.sleep(0.5)
-            # robot_dh.plot(robot_angles_rad, block=True)
+        # Verify correctness of inverse kinematics
+        Q_from_inv = self.inv_kin(ee_pos)
+        diff = np.linalg.norm(Q_from_inv[:4] - robot_angles[:4])
+        # if diff > 0.01:
+        #     raise Exception("Inverse kinematics computed incorrectly!", Q_from_inv, robot_angles)
         
-        return target_position
+        return np.hstack((ee_pos, quat_xyzw))
 
+    def get_ee_pos(self):
+        return self.get_ee_pose()[:3]
 
-    def verify_inv(self, q, p, axis):
-        limits = self.LLIM[axis]
-        if np.isnan(q).any():
-            return False
-        if 0 <= axis <= 2 and not limits[0] <= p[axis] <= limits[1]:
-            return False
-        elif 4 <= axis <= 5 and not limits[0] <= q[axis] <= limits[1]:
-            return False            
-        return True
-
+    def get_ee_quat(self):
+        return self.get_ee_pose()[3:]
+    
 
     def inv_kin(self, target_position):
 
@@ -133,78 +138,92 @@ class KochRobot:
 
         return np.array(Q)
     
+    
+    def set_ee_pose(self, pos, axis):
 
-    def write_pos(self, q):
-        q[1] *= -1  # switch first motor polarity
-        q = q / (2*np.pi) * 360
-        self.robot.follower_arms["main"].write("Goal_Position", q)
+        def verify_inv(q):
+            limits = self.LLIM[axis]
+            if np.isnan(q).any():
+                return False
+            if 0 <= axis <= 2 and not limits[0] <= pos[axis] <= limits[1]:
+                return False
+            elif 4 <= axis <= 5 and not limits[0] <= q[axis] <= limits[1]:
+                return False            
+            return True
 
+        q = self.inv_kin(pos)
+        if verify_inv(q):
+            q[1] *= -1
+            q = q / (2 * np.pi) * 360
+            self.robot.follower_arms["main"].write("Goal_Position", q)
+            return True
+        else:
+            return False
+
+    def set_gripper_open(self):
+        self.q5 = 0
+        self.set_ee_pose(self.curr_position, axis=5)
+
+    def set_gripper_close(self):
+        self.q5 = np.pi / 2
+        self.set_ee_pose(self.curr_position, axis=5)
 
     def manual_control(self):
         
-        curr_position = self.read_pos(iters=1)
-        positions = []
-        print("Current Position:", curr_position)
+        # increment
+        m = 0.05
+        # xyz = position, eeo = end-effector orientation, eeg = grip open/close
+        control_dict = {"1": {"ctrl": "xyz", "axis": 0, "direc": 1},
+                        "2": {"ctrl": "xyz", "axis": 0, "direc": -1},
+                        "3": {"ctrl": "xyz", "axis": 1, "direc": 1},
+                        "4": {"ctrl": "xyz", "axis": 1, "direc": -1},
+                        "5": {"ctrl": "xyz", "axis": 2, "direc": 1},
+                        "6": {"ctrl": "xyz", "axis": 2, "direc": -1},
+                        "7": {"ctrl": "eeo", "axis": 4, "direc": 1},
+                        "8": {"ctrl": "eeo", "axis": 4, "direc": -1},
+                        "9": {"ctrl": "eeg", "axis": 5, "direc": 1},
+                        "0": {"ctrl": "eeg", "axis": 5, "direc": -1},
+                       }
 
         def on_press(key):
-            nonlocal positions
-
-            # Position control
-            pos_keys = ["1", "3", "5", "2", "4", "6"]
-            pos_axis = [0, 1, 2, 0, 1, 2]
-            m = 0.1
-            pos_actions = [m, m, m, -m, -m, -m]
-
-            # Gripper control
-            grip_keys = ["7", "9", "8", "0"]
-            grip_axis = [4, 5, 4, 5]
-            m = 0.05
-            grip_actions = [m, m, -m, -m]            
 
             if hasattr(key, 'char'):
 
                 # Exit
                 if key.char == "x":
                     listener.stop()
-                    return positions
+                    return
                 
-                # Position controller (q1, q2, q3)
-                elif key.char in pos_keys:
-                    i = pos_keys.index(key.char)
-                    curr_position[pos_axis[i]] += pos_actions[i]
-                    print("Position:", curr_position)
-                    inv_kin = self.inv_kin(curr_position)
-                    if self.verify_inv(inv_kin, curr_position, pos_axis[i]):
-                        self.write_pos(inv_kin)
-                        positions += [curr_position]
-                    else:
-                        print("Reached limit!")
-                        curr_position[pos_axis[i]] -= pos_actions[i]
+                elif key.char in control_dict:
 
-                # Gripper controller (q4, q5)
-                elif key.char in grip_keys:
-                    i = grip_keys.index(key.char)
-                    axis = grip_axis[i]
-                    if axis == 4:
-                        self.q4 += grip_actions[i]
-                    elif axis == 5:
-                        self.q5 += grip_actions[i]
-                    inv_kin = self.inv_kin(curr_position)
-                    if self.verify_inv(inv_kin, curr_position, grip_axis[i]):
-                        self.write_pos(inv_kin)
-                    else:
-                        print("Reached limit!")
-                        if axis == 4:
-                            self.q4 -= grip_actions[i]
-                        elif axis == 5:
-                            self.q5 -= grip_actions[i]
+                    # Get control parameters
+                    control = control_dict[key.char]
+                    ctrl, axis, direc = control["ctrl"], control["axis"], control["direc"]
+
+                    # Set position
+                    if ctrl == "xyz":
+                        self.curr_position[axis] += direc * m
+                    elif ctrl == "eeo":
+                        self.q4 += direc * m
+                    elif ctrl == "eeg":
+                        self.set_gripper_open() if direc == 1 else self.set_gripper_close()
+
+                    # Revert if command has reached limit
+                    if not self.set_ee_pose(self.curr_position, axis):
+                        if ctrl == "xyz":
+                            self.curr_position[axis] -= direc * m
+                        elif ctrl == "eeo":
+                            self.q4 -= direc * m
+                        print("Limit reached!")
+
+                    print("Position:", self.curr_position)
 
         print("Axis Controls: 2<-x->1 , 4<-y->3, 6<-z->5")
         print("Gripper Controls: 7<-z->8 , 9<-g->0")
         with keyboard.Listener(on_press=on_press) as listener:
             listener.join()
 
-        return positions
+        return
 
 
     def camera_extrinsics(self, cam_node):
@@ -222,7 +241,7 @@ class KochRobot:
         for final_position in move_poses:
             
             # Generate positions from current position to end position
-            curr_position = self.read_pos(iters=1)
+            curr_position = self.get_ee_pos()
             lin_x = np.linspace(curr_position[0], final_position[0], num=100)
             lin_y = np.linspace(curr_position[1], final_position[1], num=100)
             lin_z = np.linspace(curr_position[2], final_position[2], num=100)
@@ -240,7 +259,8 @@ class KochRobot:
                     print(i)
                     time.sleep(0.5)
                     coords = cam_node.detect_end_effector()
-                    keep_coords = input("Keep? (y/n)")
+                    #keep_coords = input("Keep? (y/n)")
+                    keep_coords = "y"
                     if keep_coords == "y":
                         calibration_coords += [coords]
                         calibration_poses += [grad_poses[i]]
@@ -278,8 +298,7 @@ if __name__ == "__main__":
     try:
         do = input("Action (read=r, manual=m, calib=c):")
         if do == "r":
-            iters = int(input("Number of read iterations?"))
-            koch_robot.read_pos(iters)
+            koch_robot.get_ee_pose()
         elif do == "m":
             koch_robot.manual_control()
         elif do == "c":
